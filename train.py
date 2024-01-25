f2cb13ffc8eeff5374d0bec91fe21f9fa4705460
the0807 <the0807.eom@gmail.com>
2024-01-26 03:44:10 +0900

    test train

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ model = YOLO('yolov8n.pt')
 results = model.train(
    data='/home/students/cs/greenery/datas/data.yaml',
    imgsz=640,
-   epochs=1000,
+   epochs=100,
    batch=32,
    project='/home/students/cs/greenery/yolov8/separate_model'
 )
