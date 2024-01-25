f6c7f272483b16902752535837bca6fea785cb10
the0807 <the0807.eom@gmail.com>
2024-01-26 02:42:14 +0900

    first

---
+++ train.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# Load the model
+model = YOLO('yolov8n.pt')
+
+# Training
+results = model.train(
+   data='/home/students/cs/greenery/datas/data.yaml',
+   imgsz=640,
+   epochs=1000,
+   batch=32,
+   project='/home/students/cs/greenery/yolov8/separate_model'
+)
