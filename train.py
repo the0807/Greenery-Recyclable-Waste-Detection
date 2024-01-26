9a73cac56a3c94ec8825d423131cdb752dfcfaaf
the0807 <the0807.eom@gmail.com>
2024-01-27 08:40:13 +0900

    train1

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import comet_ml
 from ultralytics import YOLO
 
-comet_ml.init(project_name="Greenery_Recyclable_Waste_Detection")
+comet_ml.init(project_name="Greenery Recyclable Waste Detection")
 
 # Load the model
 model = YOLO('yolov8n.pt')
@@ -11,7 +11,8 @@ results = model.train(
    data='/home/students/cs/greenery/prep_datas/data.yaml',
    imgsz=640,
    epochs=1000,
-   batch=48,
+   batch=64,
    device=[0,1],
+   plots=True,
    project='/home/students/cs/greenery/yolov8/separate_model'
 )
